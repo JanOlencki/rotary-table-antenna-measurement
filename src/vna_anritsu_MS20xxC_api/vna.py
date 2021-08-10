bc9ab16f37918453b72f92e5e771964e0af7164a
Jan Olencki <jan@olencki.pl>
2021-08-11 01:58:37 +0200

    Config enviroment and tests, add simple example of listing VISA devices

--- src/vna_anritsu_MS20xxC_api/vna.py
+++ src/vna_anritsu_MS20xxC_api/vna.py
@@ -1,2 +1,7 @@
-def test_vna():
-    print("VNA")
+from typing import Tuple
+import pyvisa
+
+rm = pyvisa.ResourceManager()
+
+def list_visa_devices() -> Tuple[str, ...]:
+    return rm.list_resources()
--- tests/vna_anritsu_MS20xxC_api/test_integration_vna.py
+++ tests/vna_anritsu_MS20xxC_api/test_integration_vna.py
@@ -3,10 +3,6 @@ print(sys.path)
 
 from vna_anritsu_MS20xxC_api import vna
 
-def cli():
+def test_listing_resources():
     devices = vna.list_visa_devices()
-    print(devices)
-
-if __name__ == "__main__":
-    cli()
-    
+    assert len(devices)
