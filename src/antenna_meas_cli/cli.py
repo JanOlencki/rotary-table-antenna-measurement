fa9579dd8ef52cfa16ad0c3297d0c1a5e3b20dba
Jan Olencki <jan@olencki.pl>
2021-09-18 00:07:59 +0200

    Add saving s2p files

--- src/antenna_meas_cli/cli.py
+++ src/antenna_meas_cli/cli.py
@@ -31,13 +31,23 @@ def list_devices():
         else:
             click.echo(port_name)
 
+def add_angle_to_filename(filename: str, angle: float, angle_step:float = None) -> str:
+    precision = None
+    if angle_step is None or angle_step != round(angle_step, 0):
+        precision = 3
+    angle_str = str(round(angle, precision)).replace(".", "#")
+    return f"{filename}_{angle_str}deg"
+
 @click.command()
 @click.option("--rt-port", required=True, help="Rotary table controller COM port")
 @click.option("--rt-id", required=True, type=int, help="Rotary table ID")
 @click.option("--vna-name", required=True, help="VNA VISA resource name")
-@click.option("--angle-step", default=5, show_default=True, type=float)
+@click.option("--s2p-filename", required=True, type=click.Path(exists=False), help="S2P output filename, extension and angle suffix will be automatically added")
+@click.option("--s2p-dir", required=False, type=click.Path(exists=False), help="S2P output directory")
+@click.option("--speed", default=10, show_default=True, type=float, help="Rotational speed in RPM")
+@click.option("--angle-step", default=90, show_default=True, type=float, help="Rotary table will be rotated by angle step between measures. Rotary table rotates 360deg, but don't made measurement after returning home position.")
 @click.option("--f-show", multiple=True, type=float, help="Show live plot for given frequencies, GUI may be blocked and works unstable")
-def meas(rt_port, rt_id, vna_name, angle_step, f_show):
+def meas(rt_port, rt_id, vna_name, s2p_filename, s2p_dir, speed, angle_step, f_show):
     rt = rt_api.RotaryTable(rt_port)
     visa_rm = pyvisa.ResourceManager()
     vna = vna_api.VNA(visa_rm, vna_name)
@@ -79,11 +89,14 @@ def meas(rt_port, rt_id, vna_name, angle_step, f_show):
         with click.progressbar(angle_points, label="Making measurements",
             show_eta=True, show_pos=True) as bar:
             for angle in bar:
-                rt.send_request(rt_msg.RequestRotate(rt_id, angle, 5))
+                rt.send_request(rt_msg.RequestRotate(rt_id, angle, s2p_dir))
                 while(rt.send_request(rt_msg.RequestGetStatus(rt_id)).is_rotating):
                     time.sleep(0.2)
-                time.sleep(1)
+                time.sleep(0.5)
                 s2p = vna_single_measure(vna, test_data=True)
+                s2p.comments = f"angle={angle:f}deg"
+                filename = add_angle_to_filename(s2p_filename, angle, angle_step)                
+                s2p.write_touchstone(filename, s2p_dir, skrf_comment=False)
                 if len(f_show) > 0:
                     for i in range(len(f_show)):
                         s21db = 20*np.log10(np.abs(s2p.s[np.abs(s2p.f - f_show[i]).argmin()][1,0]))                        
@@ -91,9 +104,16 @@ def meas(rt_port, rt_id, vna_name, angle_step, f_show):
                         plots[i].set_data(angle_points[0:len(plots_data[i])], plots_data[i])
                     fig.canvas.draw()
                     fig.canvas.flush_events()
+
+        rt.send_request(rt_msg.RequestRotate(rt_id, 0, s2p_dir))
+        while(rt.send_request(rt_msg.RequestGetStatus(rt_id)).is_rotating):
+            time.sleep(0.2)
     except KeyboardInterrupt as err:
         resp = rt.send_request(rt_msg.RequestHalt(rt_id))
-        click.secho("Halting rotary table and exit")
+        click.secho("Halting rotary table...")
+        time.sleep(1)
+        resp = rt.send_request(rt_msg.RequestDisable(rt_id))
+        click.secho("Disable rotary table and exit")
         return
         pass
     if len(f_show) > 0:
@@ -106,7 +126,7 @@ def vna_single_measure(vna: vna_api.VNA, test_data=False) -> rf.Network:
         vna.start_single_sweep_await()
         s2p = vna.get_traces_data_as_s2p()
     else:
-        time.sleep(1)
+        # time.sleep(1)
         freq = rf.Frequency(1, 10, 101, 'ghz')
         s = np.random.rand(101, 2, 2) + 1j*np.random.rand(101, 2, 2)
         return rf.Network(frequency=freq, s=s, name='random values 2-port')
